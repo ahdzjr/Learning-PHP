f373188c028ebb5131ec9dc81b957febf81c068d
Albert Hernandez Jr <albert.hernandez.jr@outlook.com>
2019-07-03 01:40:56 -0500

    first commit

---
+++ learnphp.php
@@ -0,0 +1,22 @@
+<html>
+
+    <head>
+        <title>Information Gathered</title>
+    </head>
+
+    <body>
+
+      <?php
+
+      /*
+      multline
+      */
+
+      // Single line
+
+      # Another comment
+
+      echo "<p>\"Data processed\"</>;
+       ?>
+</body>
+</html>
